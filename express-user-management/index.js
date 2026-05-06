1042e1320128a21a42a3f687a9f56e3373e09010
Zabedfolio <zabedfolio@gmail.com>
2026-05-06 20:37:38 +0600

    added

--- express-user-management/index.js
+++ express-user-management/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 8000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send('hello from users world!')
@@ -14,6 +19,17 @@ const users = [
 app.get('/users', (req,res)=>{
     res.send(users)
 })
+
+app.post('/users',(req,res)=>{
+    console.log('Data in the request:', req.body);
+
+    const newUser = req.body;
+    newUser.id = users.length+1;
+    users.push(newUser);
+
+    res.send({success: true,data: newUser, message: 'post method is working'})
+})
+
 app.get('/about', (req,res)=>{
     res.send('about page')
 })
